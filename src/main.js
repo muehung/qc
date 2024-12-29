5ecd979dd253d3a976fd80f736b068d2bdec1996
muehung <hung.co24@gmail.com>
2024-12-29 16:18:06 +0800

    feat: index.html path ';'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import 'bootstrap'
+import { createApp } from 'vue';
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/assets/css/main.css'
-import '@/assets/css/navfooter.css'
-import App from './App.vue'
-import router from './router'
-import { createPinia } from 'pinia'
+import '@/assets/css/main.css';
+import '@/assets/css/navfooter.css';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
 
-const pinia = createPinia()
+const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app');
