fc7d5f98cabbe6b56eaa914de880d407a7d56318
muehung <hung.co24@gmail.com>
2024-12-30 20:31:59 +0800

    fix: router "createWebHashHistory"

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView  from "../views/HomeContentView.vue";
 import NotFound from "../views/NotFound.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
